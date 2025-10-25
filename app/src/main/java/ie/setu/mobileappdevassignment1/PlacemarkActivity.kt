7ff258616b1f5aa44420ad011c1bfdfffa41a878
cienfleur <redvelvet0214@protonmail.ch>
2025-10-25 03:37:26 +0100

    implement basic view for adding placemark, placeholder event logging

--- app/src/main/java/ie/setu/mobileappdevassignment1/PlacemarkActivity.kt
+++ app/src/main/java/ie/setu/mobileappdevassignment1/PlacemarkActivity.kt
@@ -5,20 +5,28 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import ie.setu.mobileappdevassignment1.databinding.ActivityPlacemarkBinding
 import timber.log.Timber
 import timber.log.Timber.i
 
+
 class PlacemarkActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityPlacemarkBinding
     override fun onCreate(savedInstanceState: Bundle?) {
-        Timber.plant(Timber.DebugTree())
-        i("Placemark Activity started..")
+        binding = ActivityPlacemarkBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_placemark)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        Timber.plant(Timber.DebugTree())
+        i("Placemark Activity started..")
+
+        binding.btnAdd.setOnClickListener() {
+            i("add Button Pressed")
         }
     }
 }
