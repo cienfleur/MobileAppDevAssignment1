005f7435010e9e978a16a5412a795e7cd823d32e
cienfleur <redvelvet0214@protonmail.ch>
2025-10-25 02:02:53 +0100

    implement basic event logging

--- app/src/main/java/ie/setu/mobileappdevassignment1/PlacemarkActivity.kt
+++ app/src/main/java/ie/setu/mobileappdevassignment1/PlacemarkActivity.kt
@@ -5,9 +5,13 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import timber.log.Timber
+import timber.log.Timber.i
 
 class PlacemarkActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        Timber.plant(Timber.DebugTree())
+        i("Placemark Activity started..")
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_placemark)
